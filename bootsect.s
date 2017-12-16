f5d5f924801b6bef959bd1214666c2997d94b7b6
null <641570479@qq.com>
2017-12-16 18:47:07 +0800

    1.修改源代码中的错误代码（0x70c0变成0x07c0）。2.添加文档文件夹

--- bootsect.s
+++ bootsect.s
@@ -3,8 +3,8 @@
 .global _bootstart
 
 # 16bits-real mode
-# seg:offset e.g 0x7c0:0x0000 -> seg<<4 + offset = 0x7c00
-.equ	BOOTSEG, 0X70C0
+# seg:offset e.g 0x07c0:0x0000 -> seg<<4 + offset = 0x7c00
+.equ	BOOTSEG, 0X07C0
 
 .text
 
@@ -20,12 +20,17 @@ _bootstart:
 	mov	$_string,	%bp
 	mov	$0x1301,	%ax
 	mov	$0x0007,	%bx
+    mov $strLen,    %cx
 	int	$0x10
 loop:	
 	jmp loop
 
 _string:
-	.ascii "Hello bootsect!"
+	.ascii "Hello zos"
+    .byte 13,10
+
+strLen = . - _string
+
 .= 510
 
 signature:
