47f4ea96ad4b93cd0275bb555434d306978bf4d4
null <641570479@qq.com>
2017-12-16 15:00:22 +0800

    update

--- bootsect.s
+++ bootsect.s
@@ -2,16 +2,30 @@
 
 .global _bootstart
 
-.equ	BOOTSEG, 0X70C0
-
 # 16bits-real mode
 # seg:offset e.g 0x7c0:0x0000 -> seg<<4 + offset = 0x7c00
+.equ	BOOTSEG, 0X70C0
+
+.text
 
 ljmp $BOOTSEG, $_bootstart
 
 _bootstart:
-	jmp _bootstart
+# Get cursor position
+	mov	$0x03,		%ah
+	int	$0x10
 
+	mov	$BOOTSEG,	%ax
+	mov	%ax,		%es
+	mov	$_string,	%bp
+	mov	$0x1301,	%ax
+	mov	$0x0007,	%bx
+	int	$0x10
+loop:	
+	jmp loop
+
+_string:
+	.ascii "Hello bootsect!"
 .= 510
 
 signature:
